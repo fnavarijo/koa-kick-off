15cc04cae4a7b20024984a8756633b71965d5ba7
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2020-07-20 21:50:00 -0600

    Handling koa body request

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const Koa = require('koa');
-const app = new Koa();
+const koa = require('koa');
+const koaBody = require('koa-body');
 
+const app = new koa();
+
+app.use(koaBody());
 app.use(ctx => {
-  ctx.body = 'Hello Koa';
+  const { name } = ctx.request.body;
+  ctx.body = name.toUpperCase();
 })
 
 app.listen(3000);
