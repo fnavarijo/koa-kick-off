c3e43cf863f66b31e9b7aaf9037bd0a51feed20d
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2020-07-20 22:04:53 -0600

    Adding endpoint for json and stream

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
+const fs = require('fs');
 const koa = require('koa');
+const koaBody = require('koa-body');
+
 const app = new koa();
+
+const [,, fileToRead] = process.argv;
+
+app.use((ctx, next) => {
+  if (ctx.path !== '/json') return next();
+
+  ctx.body = {
+    foo: 'bar',
+  };
+});
+
+app.use((ctx, next) => {
+  if (ctx.path !== '/stream') return next();
+
+  ctx.body = fs.createReadStream(fileToRead || 'some_file.txt');
+});
 
 app.use(ctx => {
   ctx.body = 'Hello Koa';
 })
+
 
 app.listen(3000);
